cda0122f7ac74da2cacb77a045a8910bf6f08a94
Kyle Hennig <henn.kyle@outlook.com>
2019-01-23 08:31:31 -0700

    Add graduation countdown

---
+++ index.js
@@ -0,0 +1,16 @@
+window.addEventListener('load', () => {
+    setInterval(() => {
+        const now = Date.now();
+        const grad = new Date(2022, 5, 1, 0, 0, 0, 0).getTime();
+        const timeLeft = Math.max(grad - now, 0);
+        const secondsLeft = Math.floor(timeLeft / 1000) % 60;
+        const minutesLeft = Math.floor(timeLeft / (1000 * 60)) % 60;
+        const hoursLeft = Math.floor(timeLeft / (1000 * 60 * 60)) % 24;
+        const daysLeft = Math.floor(timeLeft / (1000 * 60 * 60 * 24)) % 365;
+        const yearsLeft = Math.floor(timeLeft / (1000 * 60 * 60 * 24 * 365));
+        const countDown = `Graduating in ${yearsLeft} years, ${daysLeft} days, ${hoursLeft} hours, ${minutesLeft} minutes, ${secondsLeft} seconds.`
+
+        const graduatingElement = document.getElementById('graduating');
+        graduatingElement.innerHTML = countDown;
+    }, 1);
+});
