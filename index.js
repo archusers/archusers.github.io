ddcce42b02ab20a12deba2ccacf9645c1e57e78b
ryan <4721319+rpshukla@users.noreply.github.com>
2019-01-24 19:15:11 -0700

    Add basic typing effect

--- index.js
+++ index.js
@@ -13,4 +13,35 @@ window.addEventListener('load', () => {
         const graduatingElement = document.getElementById('graduating');
         graduatingElement.innerHTML = countDown;
     }, 1);
+
+    // Gets the text from each paragraph, then clears the contents of each
+    // paragraph so that they can be retyped one character at a time
+    let loreParagraphs = document.querySelectorAll("#lore p");
+    let textArr = [];
+    for (let i = 0; i < loreParagraphs.length; i++) {
+        textArr.push(loreParagraphs[i].innerHTML);
+        loreParagraphs[i].innerHTML = "";
+    }
+
+    // Will type one character every 30ms after 2000ms have passed
+    setTimeout(() => { setInterval(typeChar, 30); }, 2000);
+
+    /*
+     * Types one character of lore.
+     */
+    let paragraphIndex = 0;
+    let charIndex = 0;
+    function typeChar() {
+        // If we are done typing, do nothing
+        if (paragraphIndex == textArr.length) {
+	    return;
+	}
+        loreParagraphs[paragraphIndex].innerHTML += textArr[paragraphIndex].charAt(charIndex);
+        charIndex++;
+        // If we have reached the end of a paragraph, go to the beginning of the next paragraph
+        if (charIndex >= textArr[paragraphIndex].length) {
+	    charIndex = 0;
+	    paragraphIndex++;
+        }
+    }
 });
