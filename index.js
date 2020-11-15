db1ae0d2046c7e75ddc9fc48904cde79487ed301
konradstanski <kstanisz@ualberta.ca>
2020-11-14 20:31:15 -0700

    Modify year string to be correct tense

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ window.addEventListener('load', () => {
         const hoursLeft = Math.floor(timeLeft / (1000 * 60 * 60)) % 24;
         const daysLeft = Math.floor(timeLeft / (1000 * 60 * 60 * 24)) % 365;
         const yearsLeft = Math.floor(timeLeft / (1000 * 60 * 60 * 24 * 365));
-        const countDown = `Graduating in ${yearsLeft} years, ${daysLeft} days, ${hoursLeft} hours, ${minutesLeft} minutes, ${secondsLeft} seconds.`;
+        const yearStr = yearsLeft > 1 ? "years" : "year";
+        const countDown = `Graduating in ${yearsLeft} ${yearStr}, ${daysLeft} days, ${hoursLeft} hours, ${minutesLeft} minutes, ${secondsLeft} seconds.`;
 
         const graduatingElement = document.getElementById('graduating');
         graduatingElement.innerHTML = countDown;
